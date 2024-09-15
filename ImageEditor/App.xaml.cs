23c4311b53af3786697001d7eaafed565b80bff5
aieuo <35859665+aieuo@users.noreply.github.com>
2024-09-16 00:38:50 +0900

    ImageMagick 追加

--- ImageEditor/App.xaml.cs
+++ ImageEditor/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using ImageMagick;
 
 namespace ImageEditor
 {
@@ -9,5 +10,11 @@ namespace ImageEditor
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+            MagickNET.Initialize();
+        }
     }
 }
