744363c8d0e18af2886010bf9bfc0bfe2aea4d7b
aieuo <35859665+aieuo@users.noreply.github.com>
2024-09-21 22:30:09 +0900

    GravityOptionsをParameterUtilsに移動

--- ImageEditor/Models/Actions/ExtentAction.cs
+++ ImageEditor/Models/Actions/ExtentAction.cs
@@ -6,19 +6,6 @@ namespace ImageEditor.Models.Actions;
 
 internal class ExtentAction : Action
 {
-    private readonly Dictionary<Gravity, string> _allOptions = new()
-    {
-        { Gravity.Northwest, "左上" },
-        { Gravity.North, "上" },
-        { Gravity.Northeast, "右上" },
-        { Gravity.West, "左" },
-        { Gravity.Center, "中央" },
-        { Gravity.East, "右" },
-        { Gravity.Southwest, "左下" },
-        { Gravity.South, "下" },
-        { Gravity.Southeast, "右下" },
-    };
-
     public override string Name => "Extent";
 
     public override string Description => "画像に余白を追加する";
@@ -35,7 +22,7 @@ internal class ExtentAction : Action
     {
         AddParameter(new WidthAndHeightParameter("size", "サイズ", width, height));
         AddParameter(new ColorParameter("color", "余白の色", color));
-        AddParameter(new EnumParameter<Gravity>("gravity", "基準点", type, _allOptions));
+        AddParameter(new EnumParameter<Gravity>("gravity", "基準点", type, ParameterUtils.GravityOptions));
     }
 
     public override void ProcessImage(MagickImage image)
--- ImageEditor/Models/Actions/Parameters/ParameterUtils.cs
+++ ImageEditor/Models/Actions/Parameters/ParameterUtils.cs
@@ -4,6 +4,19 @@ namespace ImageEditor.Models.Actions.Parameters;
 
 public static class ParameterUtils
 {
+    public static readonly Dictionary<Gravity, string> GravityOptions = new()
+    {
+        { Gravity.Northwest, "左上" },
+        { Gravity.North, "上" },
+        { Gravity.Northeast, "右上" },
+        { Gravity.West, "左" },
+        { Gravity.Center, "中央" },
+        { Gravity.East, "右" },
+        { Gravity.Southwest, "左下" },
+        { Gravity.South, "下" },
+        { Gravity.Southeast, "右下" },
+    };
+    
     public static MagickGeometry WidthAndHeightParameterToGeometry(WidthAndHeightParameter parameter, double imageWidth,
         double imageHeight)
     {
