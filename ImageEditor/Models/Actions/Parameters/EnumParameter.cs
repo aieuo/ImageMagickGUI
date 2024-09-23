d080d361dcc5d592ad56c0b1e4013171d7f43b79
aieuo <35859665+aieuo@users.noreply.github.com>
2024-09-23 15:35:56 +0900

    EnumParameterが保存されないの修正

--- ImageEditor/Models/Actions/Parameters/EnumParameter.cs
+++ ImageEditor/Models/Actions/Parameters/EnumParameter.cs
@@ -8,16 +8,15 @@ public partial class EnumParameter<T>(string name, string description, T value, 
     [ObservableProperty]
     private T _value = value;
     
-    public new Dictionary<T, string> Options => options;
-}
-
-public class EnumParameter(string name, string description, Enum value, Dictionary<Enum, string> options)
-    : ActionParameter<Enum>(name, description, value)
-{
-    public Dictionary<Enum, string> Options => options;
+    public Dictionary<T, string> Options => options;
     
     public override string ToString()
     {
         return Value.ToString();
     }
+}
+
+public abstract class EnumParameter(string name, string description, Enum value, Dictionary<Enum, string> options)
+    : ActionParameter<Enum>(name, description, value)
+{
 }
